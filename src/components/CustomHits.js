7a795fc782b95853c43317685c919c7b9f99ff17
Bertrand d'Aure <bertrandda@laposte.net>
2020-07-25 19:37:44 +0200

    Detach zoom to vanished images

--- src/components/CustomHits.js
+++ src/components/CustomHits.js
@@ -6,15 +6,17 @@ import EmblemItem from './EmblemItem';
 
 const Hits = ({ hits }) => {
   const zoom = useRef(mediumZoom());
+  zoom.current.detach();
+
   return (
     <div className="ais-Hits">
-      <ul className="ais-Hits-list">
+      <div className="ais-Hits-list">
         {hits.map((hit, i) => (
-          <li key={`${i}-${hit.name}-item`} className="ais-Hits-item">
+          <div key={`${i}-${hit.name}-item`} className="ais-Hits-item">
             <EmblemItem hit={hit} zoom={zoom.current} />
-          </li>
+          </div>
         ))}
-      </ul>
+      </div>
     </div>
   );
 };
