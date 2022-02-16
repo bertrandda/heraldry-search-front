997d3c048d204824177ab86cdb4f707391c94909
bertrandda <bertrandda@users.noreply.github.com>
2022-02-16 23:51:53 +0100

    Fix blurry zoomed image Safari

--- src/components/CustomHits.js
+++ src/components/CustomHits.js
@@ -12,11 +12,50 @@ const Hits = ({ hits }) => {
   zoomRef.current.detach();
 
   useEffect(() => {
-    zoomRef.current.on('opened', () => {
-      document.getElementsByClassName(
-        'medium-zoom-image--opened'
-      )[0].style.visibility = 'hidden';
-    });
+    if (
+      // Hack to avoid blurry image on zoom macOS Safari & iOS/ipadOS browsers
+      navigator.userAgent.indexOf('AppleWebKit') > -1 &&
+      navigator.userAgent.search(/Chrom(e|ium)/gm) === -1
+    ) {
+      zoomRef.current.on('opened', () => {
+        const zoomedElem = document.getElementsByClassName(
+          'medium-zoom-image--opened'
+        )[1];
+        zoomedElem.style.visibility = 'hidden';
+        zoomedElem.style.position = 'fixed';
+        if (window.innerWidth < window.innerHeight) {
+          zoomedElem.style.width = '100%';
+          zoomedElem.style.removeProperty('height');
+        } else {
+          zoomedElem.style.height = '100%';
+          zoomedElem.style.removeProperty('width');
+        }
+        zoomedElem.style['max-width'] = '100%';
+        zoomedElem.style['max-height'] = '100%';
+        zoomedElem.style.top = '50%';
+        zoomedElem.style.left = '50%';
+        zoomedElem.style.transform = 'translate(-50%, -50%)';
+        zoomedElem.style.transform = zoomedElem.style.transform.replace(
+          /scale\(\d\.\d+\)/,
+          ''
+        );
+        setTimeout(() => {
+          zoomedElem.style.visibility = 'visible';
+          document.getElementsByClassName(
+            'medium-zoom-image--opened'
+          )[0].style.visibility = 'hidden';
+        }, 300);
+      });
+
+      zoomRef.current.on('close', () => {
+        document.getElementsByClassName(
+          'medium-zoom-image--opened'
+        )[0].style.visibility = 'visible';
+        document.getElementsByClassName(
+          'medium-zoom-image--opened'
+        )[1].style.visibility = 'hidden';
+      });
+    }
   }, []);
 
   return (
