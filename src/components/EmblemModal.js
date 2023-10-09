bf609735aa7b98f8a795232fbe53acc682a218cf
bertrandda <bertrandda@users.noreply.github.com>
2023-10-09 23:05:47 +0200

    Remove debug

--- src/components/EmblemModal.js
+++ src/components/EmblemModal.js
@@ -13,8 +13,6 @@ ReactModal.setAppElement('#root');
 
 const EmblemModal = () => {
   const HOST = window.location.origin;
-  // eslint-disable-next-line no-console
-  console.log(HOST, window.location);
 
   const { state, pathname } = useLocation();
   const navigate = useNavigate();
