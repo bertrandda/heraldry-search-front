086dea72c2ef9ae345103e218c93349ccdae6c90
bertrandda <bertrandda@users.noreply.github.com>
2026-05-21 00:10:20 +0200

    Remove useless config

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig(() => {
   return {
-    // https://github.com/vitejs/vite/issues/1973#issuecomment-787571499
-    define: {
-      'process.env': {},
-    },
     build: {
       outDir: 'build',
     },
