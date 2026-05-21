4b0c97371e399dd11674d2087128e1155cfebd15
bertrandda <bertrandda@users.noreply.github.com>
2026-05-21 21:41:39 +0200

    Add vite pwa plugin

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,54 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig(() => {
   return {
     build: {
       outDir: 'build',
     },
-    plugins: [react()],
+    plugins: [
+      react(),
+      VitePWA({
+        registerType: 'autoUpdate',
+        manifest: {
+          short_name: 'heraldry-search-front',
+          name: 'Armorial de france',
+          icons: [
+            {
+              src: 'favicon.ico',
+              sizes: '64x64 32x32 24x24 16x16',
+              type: 'image/x-icon',
+            },
+            {
+              src: 'icon-192.png',
+              sizes: '192x192',
+              type: 'image/png',
+            },
+            {
+              src: 'icon-512.png',
+              sizes: '512x512',
+              type: 'image/png',
+            },
+            {
+              src: 'maskable-icon-192-square.png',
+              sizes: '192x192',
+              type: 'image/png',
+              purpose: 'maskable',
+            },
+            {
+              src: 'maskable-icon-512-square.png',
+              sizes: '512x512',
+              type: 'image/png',
+              purpose: 'maskable',
+            },
+          ],
+          display: 'standalone',
+          theme_color: '#ffffff',
+          background_color: '#ffffff',
+          lang: 'fr',
+        },
+      }),
+    ],
   }
 })
